01b2f840202d43dd21e7e382bf2c576de8711eba
wujiyu <wujiyu115@gmail.com>
2017-01-05 14:48:50 +0800

    修复xclinet非百度盘链接

--- xclient.user.js
+++ xclient.user.js
@@ -15,11 +15,12 @@
     if(host == 'xclient.info'&&location.href.indexOf("a=dl")!=-1){
         var btn_down_link =document.getElementsByClassName("btn_down_link")[0];
         var code =btn_down_link .getAttribute("data-clipboard-text");
-        var url =btn_down_link .getAttribute("data-link");
-        var href_txt = "<a class='btn_down_link pop_btn' href='" + url + "#" + code + "' target='_blank'>" + '去下载' + '</a>';
-
-        var down_wrap =document.getElementsByClassName("down_wrap")[0];
-        down_wrap.innerHTML = href_txt;
+        if (code){
+            var url =btn_down_link .getAttribute("data-link");
+            var href_txt = "<a class='btn_down_link pop_btn' href='" + url + "#" + code + "' target='_blank'>" + '去下载' + '</a>';
+            console.log(code);
+            var down_wrap =document.getElementsByClassName("down_wrap")[0];
+            down_wrap.innerHTML = href_txt;
+        }
     }
 })();
-
