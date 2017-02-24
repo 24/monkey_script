6153fb35547b795e94c15018c8bee3b1103f41a3
wujiyu <wujiyu115@gmail.com>
2017-02-24 12:06:28 +0800

    知乎404

---
+++ zhihu404.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        知乎404跳转
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.zhihu.com/question/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    var link = location.href;
+    var sub_target = "https://www.zhihu.com/question/";
+    var real_url = link.replace(sub_target,"");
+    console.log(real_url);
+    window.location.href = real_url;
+})();
